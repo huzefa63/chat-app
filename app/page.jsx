205a7806e1e61adcb36785476452c83d07709dfc
learner <huzefaratlam63@gmail.com>
2025-02-26 14:59:37 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,7 @@ import { auth } from "@/lib/googleAuth";
 async function Page() {
   const res = await auth();
   console.log(res);
+  console.log('from page: ' + process.env.SOCKET_URL);
   return (
     <div className="bg-gray-300 flex-1 flex justify-center items-center">
       <div className="flex-1 h-full w-full flex justify-center">
