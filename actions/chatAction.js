e9caeed4d9494b5b281f5e670996e6ef0d33dfe2
learner <huzefaratlam63@gmail.com>
2025-02-26 16:06:57 +0530

    Update chatAction.js

--- actions/chatAction.js
+++ actions/chatAction.js
@@ -1,13 +1,19 @@
 "use server";
 import { io } from "socket.io-client";
-import {signIn, signOut} from '@/lib/googleAuth';
+import {signIn, signOut, auth} from '@/lib/googleAuth';
 // const socket = io('https://chat-api-cfe5.onrender.com');
 const socket = io(process.env.SOCKET_URL);
 export async function sendChat(data){
-    console.log('hello from server')
+    // console.log('hello from server')
+    // console.log(data.get('message'));
+    // if(data.get('message')) {
+    // socket.emit('newMessage',data.get('message'));
+    // }
+    const session = await auth();
+    if(!session) return;
     console.log(data.get('message'));
     if(data.get('message')) {
-    socket.emit('newMessage',data.get('message'));
+    socket.emit('newMessage',{name:session.user.name,photo:session.user.image,message:data.get('message')});
     }
 
 }
