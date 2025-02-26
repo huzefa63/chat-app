5a33ee2afd321ee997355a88ee418b431dcf846a
learner <huzefaratlam63@gmail.com>
2025-02-26 17:06:35 +0530

    Update chatAction.js

--- actions/chatAction.js
+++ actions/chatAction.js
@@ -1,23 +1,23 @@
 "use server";
 import { io } from "socket.io-client";
-import {signIn, signOut, auth} from '@/lib/googleAuth';
+import { signIn, signOut, auth } from '@/lib/googleAuth';
 // const socket = io('https://chat-api-cfe5.onrender.com');
 const socket = io(process.env.SOCKET_URL);
-export async function sendChat(data){
-    // console.log('hello from server')
-    // console.log(data.get('message'));
-    // if(data.get('message')) {
-    // socket.emit('newMessage',data.get('message'));
-    // }
-    
-    socket.emit('newMessage',{image:data.get('image'),name:data.get('name'),message:data.get('message')};
 
-
+export async function sendChat(data) {
+    // Emit the newMessage event with the correct object format
+    socket.emit('newMessage', {
+        image: data.get('image'),
+        name: data.get('name'),
+        message: data.get('message'),
+    });
 }
-export async function logoutAction(){
-    console.log('env: '+' '+process.env.SOCKET_URL)
+
+export async function logoutAction() {
+    console.log('env: ' + process.env.SOCKET_URL);
     await signOut();
 }
-export async function logInAction(){
-    await signIn("google", {callbackUrl:'/'});
+
+export async function logInAction() {
+    await signIn("google", { callbackUrl: '/' });
 }
