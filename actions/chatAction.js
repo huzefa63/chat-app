f84d11d67eb1348276a27513331b50f2496d1dd9
learner <huzefaratlam63@gmail.com>
2025-02-26 17:09:40 +0530

    Update chatAction.js

--- actions/chatAction.js
+++ actions/chatAction.js
@@ -6,11 +6,12 @@ const socket = io(process.env.SOCKET_URL);
 
 export async function sendChat(data) {
     // Emit the newMessage event with the correct object format
-    socket.emit('newMessage', {
-        image: data.get('image'),
-        name: data.get('name'),
-        message: data.get('message'),
-    });
+    socket.emit('newMessage', data.get('message');
+    // socket.emit('newMessage', {
+    //     image: data.get('image'),
+    //     name: data.get('name'),
+    //     message: data.get('message'),
+    // });
 }
 
 export async function logoutAction() {
