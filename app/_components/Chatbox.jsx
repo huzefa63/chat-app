aac6a3f2ab02732ad8f6c847fb2c60454a972485
learner <huzefaratlam63@gmail.com>
2025-02-26 17:28:15 +0530

    Update Chatbox.jsx

--- app/_components/Chatbox.jsx
+++ app/_components/Chatbox.jsx
@@ -6,32 +6,32 @@ import { io } from "socket.io-client"
 function Chatbox({session}) {
     let socket = useRef(null);
     const [chat,setChat] = useState([]);
-    useEffect(function(){
-        // socket.current = io('https://chat-api-cfe5.onrender.com');
-        socket.current = io(process.env.NEXT_PUBLIC_SOCKET_URL);
-        socket.current.on("newMessage", (newMessage) => {
-            console.log(newMessage.photo)
-          // setChat((messages) => [...messages, { message: newMessage }]);
-            setChat((messages) => [...messages,newMessage]);
-        });
-        return ()=>{
-            socket.current.disconnect();
-            
-        }
-    },[])
     // useEffect(function(){
     //     // socket.current = io('https://chat-api-cfe5.onrender.com');
     //     socket.current = io(process.env.NEXT_PUBLIC_SOCKET_URL);
     //     socket.current.on("newMessage", (newMessage) => {
     //         console.log(newMessage.photo)
     //       // setChat((messages) => [...messages, { message: newMessage }]);
-    //         setChat((messages) => [...messages, {message:newMessage}]);
+    //         setChat((messages) => [...messages,newMessage]);
     //     });
     //     return ()=>{
     //         socket.current.disconnect();
             
     //     }
     // },[])
+    useEffect(function(){
+        // socket.current = io('https://chat-api-cfe5.onrender.com');
+        socket.current = io(process.env.NEXT_PUBLIC_SOCKET_URL);
+        socket.current.on("newMessage", (newMessage) => {
+            console.log(newMessage.photo)
+          // setChat((messages) => [...messages, { message: newMessage }]);
+            setChat((messages) => [...messages, {message:newMessage}]);
+        });
+        return ()=>{
+            socket.current.disconnect();
+            
+        }
+    },[])
     return (
         <div className="bg-white h-[30rem] w-[80%] flex flex-col justify-between">
             <div className="border h-[90%] overflow-auto p-5">
