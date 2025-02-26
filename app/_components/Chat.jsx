64614db49a24b3161e535f98db365e524ed0ca02
learner <huzefaratlam63@gmail.com>
2025-02-26 17:18:52 +0530

    Update Chat.jsx

--- app/_components/Chat.jsx
+++ app/_components/Chat.jsx
@@ -3,8 +3,8 @@ function Chat({session,chat}) {
     return (
         <div className="border-b-2 pb-4">
             <div className="flex pb-3 items-center gap-3  ">
-                <img src={session.user.image} className="h-8 w-8 border rounded-full"></img>
-                <h1 className="text-xl tracking-widest">{session.user?.name}</h1>
+                <img src={chat.image} className="h-8 w-8 border rounded-full"></img>
+                <h1 className="text-xl tracking-widest">{chat.name}</h1>
             </div>
             <div className="flex gap-3 items-center">
                 <p className="font-bold tracking-widest"><ArrowRightCircleIcon className="size-9"/></p>
@@ -12,6 +12,18 @@ function Chat({session,chat}) {
             </div>
         </div>
     )
+    // return (
+    //     <div className="border-b-2 pb-4">
+    //         <div className="flex pb-3 items-center gap-3  ">
+    //             <img src={session.user.image} className="h-8 w-8 border rounded-full"></img>
+    //             <h1 className="text-xl tracking-widest">{session.user?.name}</h1>
+    //         </div>
+    //         <div className="flex gap-3 items-center">
+    //             <p className="font-bold tracking-widest"><ArrowRightCircleIcon className="size-9"/></p>
+    //             <p className="text-lg">{chat?.message}</p>
+    //         </div>
+    //     </div>
+    // )
 }
 
 export default Chat
