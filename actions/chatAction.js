91c37a359f3f5910398925f3a599c69ca63566bd
learner <huzefaratlam63@gmail.com>
2025-02-26 15:30:20 +0530

    Update chatAction.js

--- actions/chatAction.js
+++ actions/chatAction.js
@@ -1,7 +1,7 @@
 "use server";
 import { io } from "socket.io-client";
 import {signIn, signOut} from '@/lib/googleAuth';
-const socket = io(process.env.SOCKET_URL);
+const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL);
 export async function sendChat(data){
     console.log('hello from server')
     console.log(data.get('message'));
