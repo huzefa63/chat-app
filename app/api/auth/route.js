11614dfcc306f5a77d15c4ff0b4c5ef5fcea7355
learner <huzefaratlam63@gmail.com>
2025-04-18 11:52:02 +0530

    Update route.js

--- app/api/auth/route.js
+++ app/api/auth/route.js
@@ -1,1 +1,17 @@
+// app/api/auth/token/route.js
+
+import { getToken } from "next-auth/jwt";
+
+export async function GET(request) {
+  const token = await getToken({ req: request});
+
+  console.log("JSON Web Token", token);
+
+  return new Response(JSON.stringify(token), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
 
