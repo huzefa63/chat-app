72018df2754d9cc6e529b6ada8a19318b0fc7d2d
learner <huzefaratlam63@gmail.com>
2025-04-18 12:11:51 +0530

    Update route.js

--- app/api/auth/route.js
+++ app/api/auth/route.js
@@ -7,7 +7,7 @@ export async function GET(request) {
 
   console.log("JSON Web Token", token);
 
-  return new Response(JSON.stringify({message:"hello"}), {
+  return new Response(JSON.stringify({message:"your token",token:token}), {
     status: 200,
     headers: {
       "Content-Type": "application/json",
